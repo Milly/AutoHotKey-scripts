f2cef0a26fa010cb3ad4ab3d3544babc50e56dff
Milly <milly.ca@gmail.com>
2016-10-04 06:29:43 +0900

    Create archive.

--- BTPANConnect/build.bat
+++ BTPANConnect/build.bat
@@ -1,8 +1,21 @@
+@setlocal enableextensions
 @set src=BTPANConnect.ahk
 @set dest=BTPANConnect.exe
 @set icon=BTConnect.ico
 @set resource1=BTDisconnect.ico,icongroup,250,0
+@set archive=BTPANConnect.zip
+@set files=^
+  README.md ^
+  ..\LICENSE.txt ^
+  %dest%
 
-del /q %dest% 2>NUL
-ahk2exe /in %src% /out %dest% /icon %icon%
-ResourceHacker -addoverwrite %dest%, %dest%, %resource1%
+:clean
+del /q %dest% %archive% 2>NUL
+
+:build_exe
+ahk2exe /in %src% /out %dest% /icon %icon% || goto :eof
+ResourceHacker -addoverwrite %dest%, %dest%, %resource1% || goto :eof
+
+:archive
+7z a -- %archive% %files% >NUL || goto :eof
+
